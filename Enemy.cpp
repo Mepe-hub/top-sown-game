7d5656b137c59fe7c47b194121b8973084fb97f0
Andrebot <reinbott@gmx.de>
2021-11-15 09:31:17 +0100

    Multiple enemies

--- Enemy.cpp
+++ Enemy.cpp
@@ -31,6 +31,8 @@ void Enemy::tick(float deltaTime)
     if(!getAlive()) return;
 
     velocity = {Vector2Subtract(target->getScreenPos(), getScreenPos())};
+    if(Vector2Length(velocity) < radius) velocity = {};
+    
     BaseCharacter::tick(deltaTime);
 
     //damge to character
